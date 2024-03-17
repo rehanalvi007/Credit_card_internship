f84a6467069cda6ea0de5c5bceee5935cd04da59
rehanalvi007 <rehanalvi8527@gmail.com>
2024-03-17 11:17:31 +0530

    created new files

---
+++ app.py
@@ -0,0 +1,71 @@
+import sys
+
+from flask import Flask, request, render_template, jsonify
+from src.CreditCardDefaultPrediction.pipelines.prediction_pipeline import CustomDataset, PredictPipeline
+from src.CreditCardDefaultPrediction.exception import CustomException
+from src.CreditCardDefaultPrediction.logger import logging
+
+
+application = Flask(__name__)
+app = application
+
+
+try:
+    @app.route('/', methods=['GET'])
+    def predict_datapoint():
+        if request.method == 'GET':
+            return render_template('form.html')
+except Exception as e:
+    logging.info("An exception has occured in home route.")
+    raise CustomException(e, sys)
+
+
+try:
+    @app.route('/prediction', methods=['POST'])
+    def prediction():
+        data = CustomDataset()
+        data_frame = data.get_data_as_dataframe(LIMIT_BAL=float(request.form.get('LIMIT_BAL')),
+                                                SEX=int(request.form.get('SEX')),
+                                                EDUCATION=int(request.form.get('EDUCATION')),
+                                                MARRIAGE=int(request.form.get('MARRIAGE')),
+                                                AGE=int(request.form.get('AGE')),
+                                                PAY_0=int(request.form.get('PAY_0')),
+                                                PAY_2=int(request.form.get('PAY_2')),
+                                                PAY_3=int(request.form.get('PAY_3')),
+                                                PAY_4=int(request.form.get('PAY_4')),
+                                                PAY_5=int(request.form.get('PAY_5')),
+                                                PAY_6=int(request.form.get('PAY_6')),
+                                                BILL_AMT1=float(request.form.get('BILL_AMT1')),
+                                                BILL_AMT2=float(request.form.get('BILL_AMT2')),
+                                                BILL_AMT3=float(request.form.get('BILL_AMT3')),
+                                                BILL_AMT4=float(request.form.get('BILL_AMT4')),
+                                                BILL_AMT5=float(request.form.get('BILL_AMT5')),
+                                                BILL_AMT6=float(request.form.get('BILL_AMT6')),
+                                                PAY_AMT1=float(request.form.get('PAY_AMT1')),
+                                                PAY_AMT2=float(request.form.get('PAY_AMT2')),
+                                                PAY_AMT3=float(request.form.get('PAY_AMT3')),
+                                                PAY_AMT4=float(request.form.get('PAY_AMT4')),
+                                                PAY_AMT5=float(request.form.get('PAY_AMT5')),
+                                                PAY_AMT6=float(request.form.get('PAY_AMT6')))
+
+        predict_pipeline = PredictPipeline()
+        prediction = predict_pipeline.predict(data_frame)
+
+        result = prediction.tolist()
+        if result[0] == 1:
+            string = "This person will not pay credit next month"
+        else:
+            string = "This person will pay credit next month"
+
+        print(string)
+
+        return render_template('result.html', final_result=string)
+    
+
+except Exception as e:
+    logging.info("An exception has occured in prediction route.")
+    raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ src/CreditCardDefaultPrediction/pipelines/prediction_pipeline.py
@@ -0,0 +1,108 @@
+import os
+import sys
+
+from  src.CreditCardDefaultPrediction.utils.utils import load_object
+from src.CreditCardDefaultPrediction.logger import logging
+from src.CreditCardDefaultPrediction.exception import CustomException
+
+import pandas as pd
+import numpy as np
+
+
+class CustomDataset:
+    def __init__(self):
+        pass
+
+    def get_data_as_dataframe(self,
+                 LIMIT_BAL: float,
+                 SEX: int,
+                 EDUCATION: int,
+                 MARRIAGE: int,
+                 AGE: int,
+                 PAY_0: int,
+                 PAY_2: int,
+                 PAY_3: int,
+                 PAY_4: int,
+                 PAY_5: int,
+                 PAY_6: int,
+                 BILL_AMT1: float,
+                 BILL_AMT2: float,
+                 BILL_AMT3: float,
+                 BILL_AMT4: float,
+                 BILL_AMT5: float,
+                 BILL_AMT6: float,
+                 PAY_AMT1: float,
+                 PAY_AMT2: float,
+                 PAY_AMT3: float,
+                 PAY_AMT4: float,
+                 PAY_AMT5: float,
+                 PAY_AMT6: float):
+        try:
+            logging.info("Making dictionary")
+            custom_data_input_dict = {
+                'LIMIT_BAL': LIMIT_BAL,
+                'SEX': SEX,
+                'EDUCATION': EDUCATION,
+                'MARRIAGE': MARRIAGE,
+                'AGE': AGE,
+                'PAY_0': PAY_0,
+                'PAY_2': PAY_2,
+                'PAY_3': PAY_3,
+                'PAY_4': PAY_4,
+                'PAY_5': PAY_5,
+                'PAY_6': PAY_6,
+                'BILL_AMT1': BILL_AMT1,
+                'BILL_AMT2': BILL_AMT2,
+                'BILL_AMT3': BILL_AMT3,
+                'BILL_AMT4': BILL_AMT4,
+                'BILL_AMT5': BILL_AMT5,
+                'BILL_AMT6': BILL_AMT6,
+                'PAY_AMT1': PAY_AMT1,
+                'PAY_AMT2': PAY_AMT2,
+                'PAY_AMT3': PAY_AMT3,
+                'PAY_AMT4': PAY_AMT4,
+                'PAY_AMT5': PAY_AMT5,
+                'PAY_AMT6': PAY_AMT6
+            }
+            logging.info(f"{custom_data_input_dict}")
+            logging.info("Making Dataframe")
+            df = pd.DataFrame(custom_data_input_dict, index=[0])
+            logging.info("DataFrame gathered")
+            print(df)
+            return df
+
+        except Exception as e:
+            logging.info("An error has occurred in get_data_as_dataframe method")
+            raise CustomException(e, sys)
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+
+        try:
+
+            # Getting paths for preprocessor anr model pickled file
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            model_path = os.path.join('artifacts', 'model.pkl')
+
+            # loading data from pickled file
+            preprocessor = load_object(preprocessor_path)
+            model = load_object(model_path)
+
+            # arr = np.array(features)
+            # reshaped_arr = arr.reshape(1, -1)
+            #
+            # print(reshaped_arr)
+
+            data_scaled = preprocessor.transform(features)
+            logging.info("Data scaled using scaling")
+            prediction = model.predict(data_scaled)
+
+            return prediction
+
+        except Exception as e:
+            logging.info("Exception has occurred in prediction")
+            raise CustomException(e, sys)
