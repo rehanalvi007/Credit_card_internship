014765c99d403f8342fb49e10e779eea6910b2aa
rehanalvi007 <rehanalvi8527@gmail.com>
2024-03-21 14:12:33 +0530

    changes done

--- src/CreditCardDefaultPrediction/components/model_evaluation.py
+++ src/CreditCardDefaultPrediction/components/model_evaluation.py
@@ -1,22 +1,24 @@
 import os
 import sys
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+from src.CreditCardDefaultPrediction.logger import logging
+from src.CreditCardDefaultPrediction.exception import CustomException
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
 import numpy as np
 from src.CreditCardDefaultPrediction.utils.utils import load_object
-
 class ModelEvaluation:
     def __init__(self):
-        pass
-    
-    def eval_metrics(self, actual, pred):
-        accuracy = accuracy_score(actual, pred)  # Accuracy
-        precision = precision_score(actual, pred)  # Precision
-        recall = recall_score(actual, pred)  # Recall
-        f1 = f1_score(actual, pred)  # F1 score
-        return accuracy, precision, recall, f1
+        logging.info("evaluation started")
+
+    def eval_metrics(self,actual,pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
+        mae = mean_absolute_error(actual, pred)# here is MAE
+        r2 = r2_score(actual, pred)# here is r3 value
+        logging.info("evaluation metrics captured")
+        return rmse, mae, r2
+
 
     def initiate_model_evaluation(self, train_array, test_array):
         try:
@@ -25,21 +27,20 @@ class ModelEvaluation:
             model_path = os.path.join("artifacts", "model.pkl")
             model = load_object(model_path)
 
-            mlflow.set_registry_uri("")
+            mlflow.set_registry_uri("https://dagshub.com/rehanalvi007/Credit_card_internship.mlflow")
             
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
             print(tracking_url_type_store)
 
             with mlflow.start_run():
 
-                predicted_classes = model.predict(X_test)
+                prediction=model.predict(X_test)
 
-                (accuracy, precision, recall, f1) = self.eval_metrics(y_test, predicted_classes)
+                (rmse,mae,r2)=self.eval_metrics(y_test,prediction)
 
-                mlflow.log_metric("accuracy", accuracy)
-                mlflow.log_metric("precision", precision)
-                mlflow.log_metric("recall", recall)
-                mlflow.log_metric("f1", f1)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
 
                 #This condition is for dagshub
                 if tracking_url_type_store != "file":
